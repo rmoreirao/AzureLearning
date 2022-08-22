52e9d4cf7b2c09e658790412d8d3d81cab58d06e
Rodrigo Moreirao <rmoreirao@microsoft.com>
2022-08-22 10:09:27 -0300

    Adjusting Configurations to be used from Web App

--- 01-firstAppTodo/todo/Program.cs
+++ 01-firstAppTodo/todo/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddSingleton<ICosmosDbService>(InitializeCosmosClientInstanceAsync(builder.Configuration.GetSection("CosmosDb")).GetAwaiter().GetResult());
+builder.Services.AddSingleton<ICosmosDbService>(InitializeCosmosClientInstanceAsync(builder.Configuration).GetAwaiter().GetResult());
 
 
 var app = builder.Build();
@@ -32,12 +32,13 @@ app.Run();
 /// Creates a Cosmos DB database and a container with the specified partition key. 
 /// </summary>
 /// <returns></returns>
-static async Task<CosmosDbService> InitializeCosmosClientInstanceAsync(IConfigurationSection configurationSection)
+static async Task<CosmosDbService> InitializeCosmosClientInstanceAsync(Microsoft.Extensions.Configuration.ConfigurationManager configuration)
 {
+    var configurationSection = configuration.GetSection("CosmosDb");
     string databaseName = configurationSection.GetSection("DatabaseName").Value;
     string containerName = configurationSection.GetSection("ContainerName").Value;
-    string account = configurationSection.GetSection("Account").Value;
-    string key = configurationSection.GetSection("Key").Value;
+    string account = configuration["FirstAppCosmosDBAccount"];
+    string key = configuration["FirstAppCosmosDBKey"];
     Microsoft.Azure.Cosmos.CosmosClient client = new Microsoft.Azure.Cosmos.CosmosClient(account, key);
     CosmosDbService cosmosDbService = new CosmosDbService(client, databaseName, containerName);
     Microsoft.Azure.Cosmos.DatabaseResponse database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
